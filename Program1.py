e6b87a82ac5d64bdf0b7a42338d2cf12b34d8343
Mahathivucha <92077369+Mahathivucha@users.noreply.github.com>
2022-04-22 14:06:34 -0700

    Initial commit

---
+++ Program1.py
@@ -0,0 +1,14 @@
+class Problem:
+    def noOfSteps (self, num: int) -> int:
+        steps = 0
+        while num > 0:
+            if num % 2 == 0:
+                num /= 2 
+            else:
+                num -=1 
+            steps += 1
+        return steps
+
+ans = Problem()
+print(ans.noOfSteps(14))
+print(ans.noOfSteps(8))
