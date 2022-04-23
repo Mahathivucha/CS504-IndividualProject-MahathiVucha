993c467f2a8777bc89c89402f1b09d3b598bafbe
Mahathivucha <92077369+Mahathivucha@users.noreply.github.com>
2022-04-22 20:22:10 -0700

    Third Commit

--- Program1.py
+++ Program1.py
@@ -3,7 +3,7 @@ class Problem:
         steps = 0
         while num > 0:
             if num % 2 == 0:
-                num //= 2 
+                num /= 2 
             else:
                 num -=1 
             steps += 1
