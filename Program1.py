a5054f2ff9caac7d6dee9cc45e9b646b308b2edd
Mahathivucha <92077369+Mahathivucha@users.noreply.github.com>
2022-04-22 18:26:01 -0700

    new_changes

--- Program1.py
+++ Program1.py
@@ -8,7 +8,7 @@ class Problem:
                 num -=1 
             steps += 1
         return steps
-
+#new changes to check out
 ans = Problem()
 print(ans.noOfSteps(14))
 print(ans.noOfSteps(8))
